6322000c7f90d97b0a23adf767f90e613967f7b8
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-02 11:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+	name: "Shalu",
+	age: 22,
+	city: "Noida"
+}
+
+function getKeys(obj){
+	return Object.keys(obj)
+}
+
+console.log(getKeys(student))
